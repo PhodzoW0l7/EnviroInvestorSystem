1fc70b4e8efb96cf59e4f86aebc5f95f22ec5e1d
PhodzoDev <PhodzoW0l7>
2026-06-04 01:32:26 +0200

    added getters and setter for products

--- src/main/java/com/enviro/assessment/junior/phodzo/nagana/model/Product.java
+++ src/main/java/com/enviro/assessment/junior/phodzo/nagana/model/Product.java
@@ -12,6 +12,47 @@ public class Product {
 //    tells the database to automatically handle the creation of unique IDs using its own auto-increment feature
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public ProductType getType() {
+        return type;
+    }
+
+    public void setType(ProductType type) {
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getCurrentBalance() {
+        return currentBalance;
+    }
+
+    public void setCurrentBalance(BigDecimal currentBalance) {
+        this.currentBalance = currentBalance;
+    }
+
+    public Investor getInvestor() {
+        return investor;
+    }
+
+    public void setInvestor(Investor investor) {
+        this.investor = investor;
+    }
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private ProductType type;
