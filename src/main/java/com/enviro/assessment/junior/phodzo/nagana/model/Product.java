cac4b3d78a3bc5387e9d91ed456695ae3b44cd0f
PhodzoDev <PhodzoW0l7>
2026-06-04 11:04:56 +0200

    added Interfaces for Entities

--- src/main/java/com/enviro/assessment/junior/phodzo/nagana/model/Product.java
+++ src/main/java/com/enviro/assessment/junior/phodzo/nagana/model/Product.java
@@ -12,6 +12,16 @@ public class Product {
 //    tells the database to automatically handle the creation of unique IDs using its own auto-increment feature
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProductType type;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false,precision = 15,scale=2)
+    private BigDecimal currentBalance;
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "investors_id",nullable = false)
+    private Investor investor;
 
     public ProductType getType() {
         return type;
@@ -52,16 +62,5 @@ public class Product {
     public void setInvestor(Investor investor) {
         this.investor = investor;
     }
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private ProductType type;
-    @Column(nullable = false)
-    private String name;
-    @Column(nullable = false,precision = 15,scale=2)
-    private BigDecimal currentBalance;
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "investors_id",nullable = false)
-    private Investor investor;
 
 }
---
+++ src/main/java/com/enviro/assessment/junior/phodzo/nagana/model/WithdrawalNotice.java
@@ -0,0 +1,67 @@
+package com.enviro.assessment.junior.phodzo.nagana.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "withdrawal_notices")
+public class WithdrawalNotice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name="product_id",nullable = false)
+    private Product product;
+    @Column(nullable = false,precision = 15,scale=2)
+    private BigDecimal withdrawalAmount;
+    @Column(nullable = false)
+    private LocalDateTime requestDate;
+    @Column(nullable = false)
+    private String status;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getWithdrawalAmount() {
+        return withdrawalAmount;
+    }
+
+    public void setWithdrawalAmount(BigDecimal withdrawalAmount) {
+        this.withdrawalAmount = withdrawalAmount;
+    }
+
+    public LocalDateTime getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(LocalDateTime requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+}
